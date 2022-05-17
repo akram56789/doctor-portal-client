10a6fefed90d467f696fae14803ed847cc6454de
akram56789 <akramhossain56789@gmail>
2022-05-18 02:36:46 +0600

    login from disign done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,29 +1,117 @@
 import React from 'react';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useForm } from "react-hook-form";
 import auth from '../../firebase.init';
+import Loading from '../Shared/Loading';
+
 
 
 const Login = () => {
-  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
-    return (
-   <div className='flex justify-center items-center h-screen'>
-        <div className="card w-96 bg-base-100 shadow-xl ">
-      <div className="card-body">
-        <h2 className="text-center text-2xl font-bold uppercase  ">Login</h2>
+  const { register, formState: { errors }, handleSubmit } = useForm();
+  const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
 
-        <div className="divider">OR</div>
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useSignInWithEmailAndPassword(auth);
 
-        <button onClick={()=> signInWithGoogle()} className="btn btn-outline uppercase">continue with google</button>
-       
+  let signInError;
+
+  if(loading || gLoading){
+    return <Loading></Loading>
+  }
+
+  if(error || gError){
+    signInError= <p className="text-red-500">User Not Found ❌</p>
+  }
+
+  const onSubmit = data => {
+   console.log(data)
+   signInWithEmailAndPassword(data.email, data.password)
+
+  };
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <div className="card w-96 bg-base-100 shadow-xl ">
+        <div className="card-body">
+          <h2 className="text-center text-2xl font-bold uppercase  ">Login</h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+
+            <div class="form-control w-full max-w-xs">
+              <label class="label">
+                <span class="label-text">Email</span>
+
+              </label>
+              <input
+                type="email"
+                class="input input-bordered w-full max-w-xs"
+                {...register("email", {
+                  required: {
+                    value: true,
+                    message: 'Email is required'
+
+                  },
+                  pattern: {
+                    value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                    message: 'Provide a valid Email'
+                }
+                })}
+              />
+
+              <label className="label">
+                {errors.email?.type === 'required' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                {errors.email?.type === 'pattern' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+              </label>
+            </div>
+            <div class="form-control w-full max-w-xs">
+              <label class="label">
+                <span class="label-text">Password</span>
+
+              </label>
+              <input
+                type="password"
+                class="input input-bordered w-full max-w-xs"
+                {...register("password", {
+                  required: {
+                    value: true,
+                    message: 'Password is required'
+
+                  },
+                  minLength: {
+                    value: 6,
+                    message: 'Must be 6 characters or longer'
+                  }
+                })}
+              />
+
+              <label class="label">
+                {errors.password?.type === 'required' && <span class="label-text-alt text-red-500">{errors.password.message}</span>}
+                {errors.password?.type === 'minLength' && <span class="label-text-alt text-red-500">{errors.password.message}</span>}
+
+
+              </label>
+            </div>
+
+
+            <input className='btn w-full max-w-xs text-white' type="submit" value="login" />
+          </form>
+          {signInError}
+          <div className="divider">OR</div>
+
+          <button onClick={() => signInWithGoogle()} className="btn btn-outline uppercase">continue with google</button>
 
 
 
 
-       
+
+
+
+        </div>
       </div>
     </div>
-   </div>
-    );
+  );
 };
 
 export default Login;
---
+++ src/Pages/Shared/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        
+        <div class="h-screen flex items-center justify-center ">
+        <div class="w-40 h-40 border-t-4 border-b-4 border-gray-900 rounded-full animate-spin"></div>
+    </div>
+    );
+};
+
+export default Loading;
